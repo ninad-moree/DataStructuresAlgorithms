183d47c2471335876238c9e7ddb93c10bb33af84
Ninad More <ninadyogeshmore@gmail.com>
2023-12-16 09:34:47 +0530

    concatenation of array

---
+++ Arrays/ConcatenationOfArray.cpp
@@ -0,0 +1,28 @@
+/*
+    Given an integer array nums of length n, you want to create an array ans of length 2n 
+    where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+    Specifically, ans is the concatenation of two nums arrays.
+    Return the array ans.
+
+    Input: nums = [1,2,1]
+    Output: [1,2,1,1,2,1]
+    Explanation: The array ans is formed as follows:
+                    ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+                    ans = [1,2,1,1,2,1]
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>ans(2*n);
+        for(int i=0;i<n;i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+};
