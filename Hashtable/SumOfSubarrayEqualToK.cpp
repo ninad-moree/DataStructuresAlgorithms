1a40e6d1681660c3423f6892f1e70b764df381f1
Ninad More <ninadyogeshmore@gmail.com>
2023-12-16 10:26:57 +0530

    sum of subarray equal to k

---
+++ Hashtable/SumOfSubarrayEqualToK.cpp
@@ -0,0 +1,32 @@
+/*
+    Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+    A subarray is a contiguous non-empty sequence of elements within an array.
+
+    Input: nums = [1,1,1], k = 2
+    Output: 2
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> sumFreq;
+        int count = 0;
+        int sum = 0;
+
+        sumFreq[0] = 1;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += nums[i];
+
+            if (sumFreq.find(sum - k) != sumFreq.end()) 
+                count += sumFreq[sum - k];
+            
+            sumFreq[sum]++;
+        }
+
+        return count;
+    }
+};
