bcecb9c2418f498ea75ec868d378ca79d31bc95d
Ninad More <ninadyogeshmore@gmail.com>
2023-12-20 16:04:53 +0530

    search in rotated array

---
+++ Arrays/SearchInRotatedSortedArray.cpp
@@ -0,0 +1,48 @@
+/*
+    There is an integer array nums sorted in ascending order (with distinct values).
+    Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+    Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+    You must write an algorithm with O(log n) runtime complexity.
+
+    Example 1:
+    Input: nums = [4,5,6,7,0,1,2], target = 0
+    Output: 4
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size()-1;
+        if(target == nums[low]) 
+            return low;
+        if(target == nums[high]) 
+            return high;
+
+        while(low<high) {
+            int mid = (low + high) /2;
+
+            if(nums[mid] == target) 
+                return mid;
+            if(nums[low] <= nums[mid]) {
+                if(target >= nums[low] && target<nums[mid]) 
+                    high = mid-1;
+                else 
+                    low = mid+1;
+            }
+            else {
+                if(target>nums[mid] && target<=nums[high]) 
+                    low = mid+1;
+                else 
+                    high = mid-1;
+            }
+        }
+        if(nums[low]==target) 
+            return low;
+        else 
+            return -1;
+    }
+};
