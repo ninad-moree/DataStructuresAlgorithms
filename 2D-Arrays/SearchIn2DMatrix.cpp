b77007c2b208287030b8df8b350819b9e30ac395
Ninad More <ninadyogeshmore@gmail.com>
2023-12-20 15:33:47 +0530

    search in 2d array

---
+++ 2D-Arrays/SearchIn2DMatrix.cpp
@@ -0,0 +1,36 @@
+/*
+    You are given an m x n integer matrix matrix with the following two properties:
+    Each row is sorted in non-decreasing order.
+    The first integer of each row is greater than the last integer of the previous row.
+    Given an integer target, return true if target is in matrix or false otherwise.
+
+    You must write a solution in O(log(m * n)) time complexity.
+
+    Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+    Output: true
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int row = 0;
+        int col = n-1;
+
+        while(row<m && col>-1) {
+            int curr = matrix[row][col];
+            if(curr == target) 
+                return true;
+            if(target > curr)
+                row++;
+            else
+                col--;
+        }
+        return false;
+    }
+};
