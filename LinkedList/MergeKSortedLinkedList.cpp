8402eb7320e8dfa0dea0a07935a7bc6d8de7e7aa
Ninad More <ninadyogeshmore@gmail.com>
2023-12-22 10:58:53 +0530

    merge k sorted linked list

---
+++ LinkedList/MergeKSortedLinkedList.cpp
@@ -0,0 +1,48 @@
+/*
+    You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+    Merge all the linked-lists into one sorted linked-list and return it.
+
+    Example 1:
+    Input: lists = [[1,4,5],[1,3,4],[2,6]]
+    Output: [1,1,2,3,4,4,5,6]
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
+
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+    ListNode* mergeKLists(std::vector<ListNode*>& lists) {
+        int n = lists.size();
+        if (n == 0) return nullptr;
+
+        while (n > 1) {
+            for (int i = 0; i < n / 2; i++) {
+                lists[i] = mergeTwoLists(lists[i], lists[n - i - 1]);
+            }
+            n = (n + 1) / 2;
+        }
+
+        return lists[0];
+    }
+};
