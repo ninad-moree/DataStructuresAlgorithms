7970b4c70af1658ae8ff98f047b57cf8ff5e018b
Ninad More <ninadyogeshmore@gmail.com>
2023-12-30 15:16:52 +0530

    Peak element

--- Arrays/PeakElement.cpp
+++ Arrays/PeakElement.cpp
@@ -1,6 +1,8 @@
 /*
-    An element is called a peak element if its value is not smaller than the value of its adjacent elements(if they exists). Given an array arr[] of size N, Return the index of any one of its peak elements. 
-    Note: The generated output will always be 1 if the index that you return is correct. Otherwise output will be 0. Also, arr[] will be in ascending order before the peak element, and in descending order after that.
+    An element is called a peak element if its value is not smaller than the value of its adjacent elements(if they exists). 
+    Given an array arr[] of size N, Return the index of any one of its peak elements. 
+    Note: The generated output will always be 1 if the index that you return is correct. Otherwise output will be 0. Also, 
+    arr[] will be in ascending order before the peak element, and in descending order after that.
 
     Example 1:
     Input: arr[] = {1,2,3}
