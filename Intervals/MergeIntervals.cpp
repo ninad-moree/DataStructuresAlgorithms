06f37161d3631ec6cffc2395b8439ea4a8e9c204
Ninad More <ninadyogeshmore@gmail.com>
2024-01-02 16:43:34 +0530

    Merge Intervals Done

---
+++ Intervals/MergeIntervals.cpp
@@ -0,0 +1,33 @@
+/*
+    Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and 
+    return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+    Example 1:
+    Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+    Output: [[1,6],[8,10],[15,18]]
+    Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.empty())
+            return {};
+
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (ans.back()[1] >= intervals[i][0])
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            else
+                ans.push_back(intervals[i]);
+        }
+
+        return ans;
+    }
+};
