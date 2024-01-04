5a85bd5fedbf3b93a7a2e30e70dfedc4e39a6c2b
Ninad More <ninadyogeshmore@gmail.com>
2024-01-04 18:54:54 +0530

    Single Number 2

---
+++ Arrays/SingleNumber-2.cpp
@@ -0,0 +1,24 @@
+/*
+    Given an integer array nums where every element appears three times except for one, which appears exactly once. 
+    Find the single element and return it.
+
+    Example 1:
+    Input: nums = [2,2,3,2]
+    Output: 3
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        for(int i=2;i<nums.size();i=i+3) {
+            if(nums[i-2] != nums[i])
+                return nums[i-2];
+        }
+        return nums[nums.size()-1];
+    }
+};
