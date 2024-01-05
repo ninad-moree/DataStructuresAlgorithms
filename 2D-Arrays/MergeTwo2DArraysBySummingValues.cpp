5a6d0cb5a3e169f9a49dfb5815310ff33075c3ca
Ninad More <ninadyogeshmore@gmail.com>
2024-01-05 17:08:21 +0530

    Merge Two 2D Arrays By Summing Values

--- 2D-Arrays/MergeTwo2DArraysBySummingValues.cpp
+++ 2D-Arrays/MergeTwo2DArraysBySummingValues.cpp
@@ -5,7 +5,8 @@
     Each array contains unique ids and is sorted in ascending order by id.
     Merge the two arrays into one array that is sorted in ascending order by id, respecting the following conditions:
     Only ids that appear in at least one of the two arrays should be included in the resulting array.
-    Each id should be included only once and its value should be the sum of the values of this id in the two arrays. If the id does not exist in one of the two arrays then its value in that array is considered to be 0.
+    Each id should be included only once and its value should be the sum of the values of this id in the two arrays. 
+    If the id does not exist in one of the two arrays then its value in that array is considered to be 0.
     Return the resulting array. The returned array must be sorted in ascending order by id.
 
     Example 1:
