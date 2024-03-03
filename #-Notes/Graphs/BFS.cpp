f3e2a5365f915b8597ecf722ce804a0d837f1220
ninad-moree <ninadyogeshmore@gmail.com>
2024-03-03 15:06:42 +0530

    BFS Code Added

---
+++ #-Notes/Graphs/BFS.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph {
+    public:
+    int nodes;
+    vector<vector<int>> adjList ;
+
+    Graph(int n) {
+        nodes = n;
+        adjList = {};
+    }
+
+    void addEdge(int u, int v) {
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+
+
+    void BFS(int startNode) {
+        vector<bool> visited(nodes , false); 
+        queue<int> q;
+
+        visited[startNode] = true;
+        q.push(startNode);
+
+        while (!q.empty()) {
+            int curr = q.front();
+            cout << curr << " ";
+            q.pop();
+
+            for(int i : adjList[curr]) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    q.push(i);
+                }
+            }
+        }
+        cout<<endl;
+    }
+
+};
