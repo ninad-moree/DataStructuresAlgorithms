76fc9b55ef241a54c158894b6e8dff58ef53c429
ninad-moree <ninadyogeshmore@gmail.com>
2024-04-06 18:38:48 +0530

    Minimum Remove to Make Valid Parentheses

---
+++ Stack/1249-MinimumRemoveMakeValidParentheses.cpp
@@ -0,0 +1,51 @@
+/*
+    Given a string s of '(' , ')' and lowercase English characters.
+    Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting 
+    parentheses string is valid and return any valid string.
+    Formally, a parentheses string is valid if and only if:
+    It is the empty string, contains only lowercase characters, or
+    It can be written as AB (A concatenated with B), where A and B are valid strings, or
+    It can be written as (A), where A is a valid string.
+    
+    Example 1:
+    Input: s = "lee(t(c)o)de)"
+    Output: "lee(t(c)o)de"
+    Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        int n = s.size();
+        string result = "";
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else if (s[i] == ')') {
+                if (!st.empty()) {
+                    st.pop();
+                } else {
+                    s[i] = '*'; 
+                }
+            }
+        }
+
+        while (!st.empty()) {
+            s[st.top()] = '*';
+            st.pop();
+        }
+
+        for (char c : s) {
+            if (c != '*') {
+                result += c;
+            }
+        }
+
+        return result;
+    }
+};
