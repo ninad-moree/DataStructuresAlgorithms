4d01d3f8f7f937f33bb830b1186789e2dd5b2634
ninad-moree <ninadyogeshmore@gmail.com>
2024-04-13 14:38:32 +0530

    Merge Two 2D Arrays by Summing Values

--- Multidimensional Arrays/2570-MergeTwo2DArraysBySummingValues.cpp
+++ Multidimensional Arrays/2570-MergeTwo2DArraysBySummingValues.cpp
@@ -27,25 +27,20 @@ public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
         vector<vector<int>> ans;
 
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-
         int i = 0;
         int j = 0;
-         
+
         while(i<nums1.size() && j<nums2.size()) {
             if(nums1[i][0] == nums2[j][0]) {
-                ans.push_back({nums1[i][0], (nums1[i][1] + nums2[j][1])});
+                ans.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
                 i++;
                 j++;
-            }
-            else if(nums1[i][0] < nums2[j][0]) {
-                ans.push_back({nums1[i][0], nums1[i][1]});
-                i++;
-            }
-            else if(nums1[i][0] > nums2[j][0]) {
+            } else if(nums1[i][0] > nums2[j][0]) {
                 ans.push_back({nums2[j][0], nums2[j][1]});
                 j++;
+            } else if(nums1[i][0] < nums2[j][0]) {
+                ans.push_back({nums1[i][0], nums1[i][1]});
+                i++;
             }
         }
 
