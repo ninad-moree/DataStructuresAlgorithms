a420e8846b8b739a16ca58640c0353ae29884e7d
ninad-moree <ninadyogeshmore@gmail.com>
2024-05-04 12:16:13 +0530

    Boats to Save People

--- Arrays/0881-BoatsToSavePeople.cpp
+++ Arrays/0881-BoatsToSavePeople.cpp
@@ -1,9 +1,7 @@
 /*
-    You are given an array people where people[i] is the weight of the ith person, 
-    and an infinite number of boats where each boat can carry a maximum weight of limit. 
-    Each boat carries at most two people at the same time, provided the sum of the weight 
-    of those people is at most limit.
-    Return the minimum number of boats to carry every given person.
+    You are given an array people where people[i] is the weight of the ith person, and an infinite number of boats where each boat 
+    can carry a maximum weight of limit. Each boat carries at most two people at the same time, provided the sum of the weight  of 
+    those people is at most limit. Return the minimum number of boats to carry every given person.
 
     Example 2:
     Input: people = [3,2,2,1], limit = 3
