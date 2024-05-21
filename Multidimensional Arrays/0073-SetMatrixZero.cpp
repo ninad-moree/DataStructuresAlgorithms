46869577c26409e030d39e8931e15432df3b6dab
ninad-moree <ninadyogeshmore@gmail.com>
2024-05-21 17:18:39 +0530

    Set Matrix Zeroes

--- Multidimensional Arrays/0073-SetMatrixZero.cpp
+++ Multidimensional Arrays/0073-SetMatrixZero.cpp
@@ -13,36 +13,29 @@ using namespace std;
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int col = 1;
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
+        vector<int> rows(n, 0);
+        vector<int> cols(m, 0);
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
                 if(matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    if(j!=0)
-                        matrix[0][j] = 0;
-                    else 
-                        col=0;
+                    rows[i] = 1;
+                    cols[j] = 1;
                 }
             }
         }
 
-        for(int i=1; i<m;i++) {
-            for(int j=1;j<n;j++) {
-                if(matrix[0][j]==0 || matrix[i][0]==0) 
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(rows[i] == 1)
+                    matrix[i][j] = 0;
+                
+                if(cols[j] == 1)
                     matrix[i][j] = 0;
             }
-        }
-
-        if(matrix[0][0]==0){
-            for(int j=0;j<n;j++)
-                matrix[0][j]=0;
-        }
-        if(col==0){
-            for(int i=0;i<m;i++)
-                matrix[i][0]=0;
         }
     }
 };
