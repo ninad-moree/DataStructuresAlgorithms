c7a71dcf2ca5e09760b8ae71086f9a91a04056ba
ninad-moree <ninadyogeshmore@gmail.com>
2024-06-14 11:01:14 +0530

    Sort Colors

--- Arrays/0075-SortColors.cpp
+++ Arrays/0075-SortColors.cpp
@@ -13,30 +13,23 @@ using namespace std;
 
 class Solution {
 public:
-    int partition(vector<int>& arr, int low, int high)
-    {
-        int pivot = arr[high];
-        int i = (low - 1);
-    
-        for (int j = low; j <= high - 1; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                swap(arr[i], arr[j]);
+    void sortColors(vector<int>& nums) {
+        int a = 0;
+        int b = 0;
+        int c = nums.size()-1;
+
+        while(b<=c) {
+            if(nums[b] == 0) {
+                swap(nums[a], nums[b]);
+                a++;
+                b++;
+            }
+            else if(nums[b] == 1) 
+                b++;
+            else {
+                swap(nums[b], nums[c]);
+                c--;
             }
         }
-        swap(arr[i + 1], arr[high]);
-        return (i + 1);
-    }
-
-    void quickSort(vector<int>& arr, int low, int high)
-    {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
-    void sortColors(vector<int>& nums) {
-        quickSort(nums, 0, nums.size()-1);
     }
 };
