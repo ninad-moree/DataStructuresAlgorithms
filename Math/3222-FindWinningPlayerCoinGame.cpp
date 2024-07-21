f685d796176300911145339311b7228c24bd04c0
ninad-moree <ninadyogeshmore@gmail.com>
2024-07-21 11:50:25 +0530

    Find the Winning Player in Coin Game

--- Math/3222-FindWinningPlayerCoinGame.cpp
+++ Math/3222-FindWinningPlayerCoinGame.cpp
@@ -1,8 +1,7 @@
 /*
-    You are given two positive integers x and y, denoting the number of coins with values 75 and 10 respectively.
-    Alice and Bob are playing a game. Each turn, starting with Alice, the player must pick up coins with a total 
-    value 115. If the player is unable to do so, they lose the game.
-    Return the name of the player who wins the game if both players play optimally.
+    You are given two positive integers x and y, denoting the number of coins with values 75 and 10 respectively. Alice and Bob are playing a 
+    game. Each turn, starting with Alice, the player must pick up coins with a total value 115. If the player is unable to do so, they lose the 
+    game. Return the name of the player who wins the game if both players play optimally.
 
     Example 1:
     Input: x = 2, y = 7
