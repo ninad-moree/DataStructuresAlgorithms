bcbef0ee54b88bfe7ffe1f2a22c76feb6e16c24e
ninad-moree <ninadyogeshmore@gmail.com>
2024-08-11 11:48:06 +0530

    Counting Bits

--- BitManipulation/0338-CountBits.cpp
+++ BitManipulation/0338-CountBits.cpp
@@ -44,4 +44,18 @@ public:
         }
         return ans;
     }
+
+    // ****************************************** SECOND SOLUTION ****************************************** //
+    vector<int> countBits1(int n) {
+        vector<int> ans(n+1, 0);
+
+        for(int i=1; i<=n; i++) {
+            if(i%2 == 0)
+                ans[i] = ans[i/2];
+            else
+                ans[i] = ans[i-1] + 1;
+        }
+
+        return ans;
+    } 
 };
