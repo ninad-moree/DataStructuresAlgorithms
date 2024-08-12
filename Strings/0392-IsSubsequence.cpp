2817723e1a62dd36bcb3825b4dc20a02c9a8659d
ninad-moree <ninadyogeshmore@gmail.com>
2024-08-12 19:33:22 +0530

    Is Subsequence

--- Strings/0392-IsSubsequence.cpp
+++ Strings/0392-IsSubsequence.cpp
@@ -17,16 +17,15 @@ public:
     bool isSubsequence(string s, string t) {
         int i=0;
         int j=0;
-        int n = s.size();
-        int m = t.size();
 
-        while(i<n && j<m) {
-            if(s[i]==t[j])
+        while(i<s.size() && j<t.size()) {
+            if(s[i] == t[j]){
                 i++;
-            j++;
+                j++;
+            } else
+                j++;
         }
-        bool ans = false;
-        if(i == n) ans = true;
-        return ans;
+
+        return i == s.size();
     }
 };
