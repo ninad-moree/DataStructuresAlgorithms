5829964ef19848e3ea46e0ae472b5c014770062b
ninad-moree <ninadyogeshmore@gmail.com>
2024-08-12 15:06:52 +0530

    Kth Largest Element in a Stream

--- Heap-PriorityQueue/0703-KthLargestElementInStream.cpp
+++ Heap-PriorityQueue/0703-KthLargestElementInStream.cpp
@@ -1,13 +1,10 @@
 /*
-    Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, 
-    not the kth distinct element.
-    Implement KthLargest class:
-    KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+    Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct 
+    element. Implement KthLargest class: KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
     int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in 
     the stream.
     
     Example 1:
-
     Input:
     ["KthLargest", "add", "add", "add", "add", "add"]
     [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
@@ -26,30 +23,25 @@
 using namespace std;
 
 class KthLargest {
-    int k;
-    priority_queue<int, vector<int>, greater<int>> pq; 
-    
 public:
+    int K;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
     KthLargest(int k, vector<int>& nums) {
-        this->k = k;
-        for (int num : nums) {
-            pq.push(num);
-            if (pq.size() > k) 
+        K = k;
+
+        for(int i=0; i<nums.size(); i++) {
+            pq.push(nums[i]);
+            if(pq.size() > K)
                 pq.pop();
         }
     }
     
     int add(int val) {
         pq.push(val);
-        if (pq.size() > k) 
+        if(pq.size() > K)
             pq.pop();
         
         return pq.top();
     }
 };
-
-/*
-    Your KthLargest object will be instantiated and called as such:
-    KthLargest* obj = new KthLargest(k, nums);
-    int param_1 = obj->add(val);
-*/
