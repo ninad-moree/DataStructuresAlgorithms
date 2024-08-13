14c0d0c4c64d599819b42360141bfa9b5d74c93f
ninad-moree <ninadyogeshmore@gmail.com>
2024-08-13 10:38:11 +0530

    Longest Consecutive Sequence

--- Arrays/0128-LongestConsecutiveSequence.cpp
+++ Arrays/0128-LongestConsecutiveSequence.cpp
@@ -15,28 +15,24 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int cnt = 1;
-        int res = INT_MIN;
 
-        if(nums.size()==0) 
+        if(nums.size() == 0)
             return 0;
 
-        for(int i=0;i<nums.size()-1;i++) {
-            if(nums[i]==nums[i+1])
+        int ans = INT_MIN;
+        int curr = 1;
+
+        for(int i=0; i<nums.size()-1; i++) {
+            if(nums[i] == nums[i+1])
                 continue;
-            else if(nums[i]-nums[i+1] == -1){
-                cnt++;
-                res = max(res,cnt);
-            }
-            else
-                cnt=1;
+            else if(nums[i] - nums[i+1] == -1) {
+                curr++;
+                ans = max(ans, curr);
+            } else 
+                curr = 1;
         }
 
-        res = max(res,cnt);
-
-        if(res == INT_MIN)
-            return 0;
-            
-        return res;
+        ans = max(ans, curr);
+        return ans == INT_MIN ? 0 : ans;
     }
 };
