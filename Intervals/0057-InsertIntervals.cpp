07a7c936287907338593d11a2dafd90e4b775752
ninad-moree <ninadyogeshmore@gmail.com>
2024-08-14 15:50:49 +0530

    Insert Interval

--- Intervals/0057-InsertIntervals.cpp
+++ Intervals/0057-InsertIntervals.cpp
@@ -17,32 +17,20 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> result;
-        int i = 0;
-        int n = intervals.size();
+        vector<vector<int>> ans;
 
-        // Adding intervals that come before newInterval
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            result.push_back(intervals[i]);
-            i++;
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+
+        ans.push_back(intervals[0]);
+
+        for(int i=1; i<intervals.size(); i++) {
+            if(ans.back()[1] >= intervals[i][0])
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            else
+                ans.push_back(intervals[i]);
         }
 
-        // Merging overlapping intervals
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-
-        // Adding the merged interval
-        result.push_back(newInterval);
-
-        // Adding intervals that come after newInterval
-        while (i < n) {
-            result.push_back(intervals[i]);
-            i++;
-        }
-
-        return result;
+        return ans;
     }
 };
