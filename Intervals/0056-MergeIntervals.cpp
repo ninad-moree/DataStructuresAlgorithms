e9f3a7888073c3666864dedcd16f4506511f88b7
ninad-moree <ninadyogeshmore@gmail.com>
2024-08-14 11:26:17 +0530

    Merge Intervals

--- Intervals/0056-MergeIntervals.cpp
+++ Intervals/0056-MergeIntervals.cpp
@@ -14,15 +14,16 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty())
-            return {};
-
         sort(intervals.begin(), intervals.end());
+
+        if(intervals.size() <= 1)
+            return intervals;
+
         vector<vector<int>> ans;
         ans.push_back(intervals[0]);
 
-        for (int i = 1; i < intervals.size(); i++) {
-            if (ans.back()[1] >= intervals[i][0])
+        for(int i=1; i<intervals.size(); i++) {
+            if(ans.back()[1] >= intervals[i][0])
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             else
                 ans.push_back(intervals[i]);
