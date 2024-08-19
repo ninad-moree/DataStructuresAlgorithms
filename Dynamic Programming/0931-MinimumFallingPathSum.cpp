173dde484bf3dffa0e97709d085547a11511fc99
ninad-moree <ninadyogeshmore@gmail.com>
2024-08-19 11:02:42 +0530

    Minimum Falling Path Sum

--- Dynamic Programming/0931-MinimumFallingPathSum.cpp
+++ Dynamic Programming/0931-MinimumFallingPathSum.cpp
@@ -1,8 +1,7 @@
 /*
-    Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
-    A falling path starts at any element in the first row and chooses the element in the next row that 
-    is either directly below or diagonally left/right. Specifically, the next element from position 
-    (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+    Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix. A falling path starts at any element in the 
+    first row and chooses the element in the next row that  is either directly below or diagonally left/right. Specifically, the next element from 
+    position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
 
     Example 1:
     Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
