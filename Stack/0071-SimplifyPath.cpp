be502f589bfbb679ea89aaa41ca30521d3169274
ninad-moree <ninadyogeshmore@gmail.com>
2024-09-07 10:59:13 +0530

    Simplify Path

---
+++ Stack/0071-SimplifyPath.cpp
@@ -0,0 +1,60 @@
+/*
+    You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its 
+    simplified canonical path.The rules of a Unix-style file system are as follows:
+    A single period '.' represents the current directory.
+    A double period '..' represents the previous/parent directory.
+    Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
+    Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid 
+    directory or file names.
+    The simplified canonical path should follow these rules:
+    The path must start with a single slash '/'.
+    Directories within the path must be separated by exactly one slash '/'.
+    The path must not end with a slash '/', unless it is the root directory.
+    The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
+    Return the simplified canonical path.
+
+    Example 1:
+    Input: path = "/home/user/Documents/../Pictures"
+    Output: "/home/user/Pictures"
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+
+        vector<string> words;
+        stringstream ss(path);
+        string token;
+
+        while(getline(ss, token, '/'))
+            words.push_back(token);
+        
+        for(auto i : words)
+            cout<<i<<"-";
+
+        string ans = "";
+        for(auto i : words) {
+            if(i == "" || i == ".")
+                continue;
+            else if(i == "..") {
+                if(!st.empty())
+                    st.pop();
+            } else 
+                st.push(i);
+        }
+
+        while(!st.empty()) {
+            string str = "/" + st.top();
+            ans = str + ans;
+            st.pop();
+        }
+
+        if(ans == "")
+            return "/";
+        return ans;
+    }
+};
