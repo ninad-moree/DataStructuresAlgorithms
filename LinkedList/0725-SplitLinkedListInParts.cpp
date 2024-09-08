2f824d33c06a8d93d7dbb8a1e05181fadaf56017
ninad-moree <ninadyogeshmore@gmail.com>
2024-09-08 20:42:23 +0530

    Split Linked List in Parts

--- LinkedList/0725-SplitLinkedListInParts.cpp
+++ LinkedList/0725-SplitLinkedListInParts.cpp
@@ -1,15 +1,13 @@
 /*
-    Given the head of a singly linked list and an integer k, split the linked list into k 
-    consecutive linked list parts.
-    The length of each part should be as equal as possible: no two parts should have a size 
-    differing by more than one. This may lead to some parts being null.
-    The parts should be in the order of occurrence in the input list, and parts occurring 
-    earlier should always have a size greater than or equal to parts occurring later.
+    Given the head of a singly linked list and an integer k, split the linked list into k consecutive linked list parts. The length of each part should
+    be as equal as possible: no two parts should have a size  differing by more than one. This may lead to some parts being null.  The parts should be 
+    in the order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal to parts occurring later.
     Return an array of the k parts.
 
     Input: head = [1,2,3,4,5,6,7,8,9,10], k = 3
     Output: [[1,2,3,4],[5,6,7],[8,9,10]]
-    Explanation: The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later parts.
+    Explanation: The input has been split into consecutive parts with size difference at most 1, and earlier parts are a larger size than the later 
+    parts.
 */
 
 #include<bits/stdc++.h>
@@ -48,9 +46,8 @@ public:
 
             result.push_back(current);
 
-            for (int j = 0; j < partSize - 1; j++) {
+            for (int j = 0; j < partSize - 1; j++) 
                 current = current->next;
-            }
 
             ListNode* temp = current->next;
             current->next = nullptr;
