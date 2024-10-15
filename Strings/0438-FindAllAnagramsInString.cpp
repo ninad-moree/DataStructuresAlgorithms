ad255a63d66b65f05b0b0edf27a9c5b810c281df
ninad-moree <ninadyogeshmore@gmail.com>
2024-10-15 10:03:36 +0530

    Find All Anagrams in a String

--- Strings/0438-FindAllAnagramsInString.cpp
+++ Strings/0438-FindAllAnagramsInString.cpp
@@ -18,30 +18,27 @@ public:
         vector<int> ans;
         int n = s.size();
         int m = p.size();
+        
         if (n < m) 
             return ans;
 
         vector<int> p_freq(26, 0), s_freq(26, 0);
 
-        for (char ch : p) {
+        for (char ch : p) 
             p_freq[ch - 'a']++;
-        }
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) 
             s_freq[s[i] - 'a']++;
-        }
-
-        if (p_freq == s_freq) {
+        
+        if (p_freq == s_freq) 
             ans.push_back(0);
-        }
 
         for (int i = m; i < n; i++) {
             s_freq[s[i - m] - 'a']--; 
             s_freq[s[i] - 'a']++;    
 
-            if (p_freq == s_freq) {
+            if (p_freq == s_freq) 
                 ans.push_back(i - m + 1);
-            }
         }
 
         return ans;
