aeb7cb79f0a92426343eae81ad864b60f2348ac3
ninad-moree <ninadyogeshmore@gmail.com>
2024-12-22 12:51:15 +0530

    Linked List files added

--- !-NotLeetCode/LinkedList/RemoveLoopInLinkedList.cpp
+++ !-NotLeetCode/LinkedList/RemoveLoopInLinkedList.cpp
@@ -1,16 +1,16 @@
 /*
-Given a linked list of N nodes such that it may contain a loop.
-A loop here means that the last node of the link list is connected to the node at position X(1-based index). If the link list does not have any loop, X=0.
-Remove the loop from the linked list, if it is present, i.e. unlink the last node which is forming the loop.
+    Given a linked list of N nodes such that it may contain a loop.
+    A loop here means that the last node of the link list is connected to the node at position X(1-based index). If the link list does not have any loop, X=0.
+    Remove the loop from the linked list, if it is present, i.e. unlink the last node which is forming the loop.
 
-Example 1:
-Input: value[] = {1,3,4} 
-Output: 1
-Explanation: The link list looks like
-1 -> 3 -> 4
-     ^    |
-     |____|    
-A loop is present. If you remove it successfully, the answer will be 1. 
+    Example 1:
+    Input: value[] = {1,3,4} 
+    Output: 1
+    Explanation: The link list looks like
+    1 -> 3 -> 4
+        ^    |
+        |____|    
+    A loop is present. If you remove it successfully, the answer will be 1. 
 */
 
 #include<bits/stdc++.h>
