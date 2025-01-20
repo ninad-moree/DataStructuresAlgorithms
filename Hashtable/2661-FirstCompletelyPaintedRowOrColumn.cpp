39bdbca17d98a36220d828164140560998e107fc
ninad-moree <ninadyogeshmore@gmail.com>
2025-01-20 11:08:00 +0530

    First Completely Painted Row or Column

--- Hashtable/2661-FirstCompletelyPaintedRowOrColumn.cpp
+++ Hashtable/2661-FirstCompletelyPaintedRowOrColumn.cpp
@@ -1,11 +1,9 @@
 /*
-    You are given a 0-indexed integer array arr, and an m x n integer matrix mat. arr and mat both contain all the integers in the 
-    range [1, m * n].
-    Go through each index i in arr starting from index 0 and paint the cell in mat containing the integer arr[i].
-    Return the smallest index i at which either a row or a column will be completely painted in mat.
+    You are given a 0-indexed integer array arr, and an m x n integer matrix mat. arr and mat both contain all the integers in the range [1, m * n].
+    Go through each index i in arr starting from index 0 and paint the cell in mat containing the integer arr[i]. Return the smallest index i at which 
+    either a row or a column will be completely painted in mat.
 
     Example 1:
-    image explanation for example 1
     Input: arr = [1,3,4,2], mat = [[1,4],[2,3]]
     Output: 2
     Explanation: The moves are shown in order, and both the first row and second column of the matrix become fully painted at arr[2].
