e0841c10fe1480a512670bf6b7635793db84ad5c
ninad-moree <ninadyogeshmore@gmail.com>
2025-04-21 11:39:48 +0530

    Sqrt(x)

---
+++ Arrays/0069-Sqrt(x).cpp
@@ -0,0 +1,38 @@
+/*
+    Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as 
+    well. You must not use any built-in exponent function or operator. For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+    Example 1:
+    Input: x = 4
+    Output: 2
+    Explanation: The square root of 4 is 2, so we return 2.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 0;
+        int high = x;
+        long long int mid = low + (high - low)/2;
+
+        int ans = -1;
+
+        while(low <= high) {
+            if(mid * mid == x)
+                return mid;
+            else if(mid * mid > x)
+                high = mid - 1;
+            else {
+                ans = mid;
+                low = mid + 1;
+            }
+
+            mid = low + (high - low)/2;
+        }
+
+        return ans;
+    }
+};
