9a62a432b63b55275f9e6c3c236f4baa206dc7ba
ninad-moree <ninadyogeshmore@gmail.com>
2025-04-22 10:48:34 +0530

    Valid Anagram

--- Strings/0242-ValidAnagram.cpp
+++ Strings/0242-ValidAnagram.cpp
@@ -15,17 +15,7 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
-        
-        int n = s.size();
-        int m = t.size();
-        if(n!=m) return false;
 
-        bool ans = true;
-
-        for(int i=0;i<n;i++) {
-            if(s[i]!=t[i])
-                ans = false;
-        }
-        return ans;
+        return s == t;
     }
 };
