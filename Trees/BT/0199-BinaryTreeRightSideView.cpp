06edbc3394be9d57f3bb30be9f6f4244d9045c10
ninad-moree <ninadyogeshmore@gmail.com>
2025-06-04 12:14:55 +0530

    Binary Tree Right Side View

--- Trees/BT/0199-BinaryTreeRightSideView.cpp
+++ Trees/BT/0199-BinaryTreeRightSideView.cpp
@@ -22,20 +22,58 @@ struct TreeNode {
 
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int>& ans, int lvl) {
-        if(root == NULL) 
-            return;
-
-        if(lvl == ans.size())
-            ans.push_back(root->val);
-
-        solve(root->right, ans, lvl+1);
-        solve(root->left, ans, lvl+1);
-    }
-    
     vector<int> rightSideView(TreeNode* root) {
+        if(!root)
+            return {};
+
+        queue<TreeNode*> q;
+        vector<vector<int>> bfs;
         vector<int> ans;
-        solve(root, ans, 0);
+
+        q.push(root);
+
+        while(!q.empty()) {
+            int s = q.size();
+            vector<int> lvl;
+
+            for(int i=0; i<s; i++) {
+                auto f = q.front();
+                q.pop();
+
+                lvl.push_back(f->val);
+
+                if(f->left != NULL)
+                    q.push(f->left);
+                if(f->right != NULL)
+                    q.push(f->right);
+            }
+
+            bfs.push_back(lvl);
+        }
+
+        for(auto i : bfs) 
+            ans.push_back(i[i.size()-1]);
+
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     void solve(TreeNode* root, vector<int>& ans, int lvl) {
+//         if(root == NULL) 
+//             return;
+
+//         if(lvl == ans.size())
+//             ans.push_back(root->val);
+
+//         solve(root->right, ans, lvl+1);
+//         solve(root->left, ans, lvl+1);
+//     }
+    
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> ans;
+//         solve(root, ans, 0);
+//         return ans;
+//     }
+// };
