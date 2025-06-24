7d74f770109b351de8e26fb71598a568ee352eae
ninad-moree <ninadyogeshmore@gmail.com>
2025-06-24 14:33:58 +0530

    Majority Element

--- Arrays/0169-MajorityElement.cpp
+++ Arrays/0169-MajorityElement.cpp
@@ -1,6 +1,5 @@
 /*
-    Given an array nums of size n, return the majority element.
-    The majority element is the element that appears more than ⌊n / 2⌋ times. 
+    Given an array nums of size n, return the majority element. The majority element is the element that appears more than ⌊n / 2⌋ times. 
     You may assume that the majority element always exists in the array.
 
     Example 1:
@@ -11,52 +10,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Solution{
-  public:
-    int majorityElement(int a[], int size) {
-        int candidate = -1;
-        int count = 0;      
-        int n = size;
-
-        for (int i = 0; i < n; i++) {
-            if (count == 0) {
-                candidate = a[i];
-                count = 1;
-            } else if (a[i] == candidate)
-                count++; 
-            else
-                count--; 
-        }
-    
-        count = 0; 
-        for (int i = 0; i < n; i++) {
-            if (a[i] == candidate)
-                count++;
-        }
-    
-        if (count > n / 2)
-            return candidate;
-    
-        return -1;
-    }
-    
+class Solution {
+public:
     int majorityElement(vector<int>& nums) {
-        // sort(nums.begin(), nums.end());
-        // int n = nums.size();
-        // return nums[n/2];
+        int ele;
+        int cnt = 0;
+        int n = nums.size();
 
-        unordered_map<int, int> mp;
-
-        for(int i=0;i<nums.size();i++) 
-            mp[nums[i]]++;
-        
-        int n = nums.size() / 2;
-
-        for(auto i : mp) {
-            if(i.second > n)
-                return i.first;
+        for(int i=0; i<n; i++) {
+            if(cnt == 0) {
+                ele = nums[i];
+                cnt = 1;
+            } else if(nums[i] == ele)
+                cnt++;
+            else
+                cnt--;
         }
 
-        return -1;
+        return ele;
     }
 };
