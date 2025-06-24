55342edb0811d3bd74ed7c30fb5240a7abcf0443
ninad-moree <ninadyogeshmore@gmail.com>
2025-06-24 15:30:35 +0530

    Next Permutation

--- Arrays/0031-NextPermutation.cpp
+++ Arrays/0031-NextPermutation.cpp
@@ -19,28 +19,27 @@ using namespace std;
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int pivot = -1;
+        int idx = -1; // breakPoint
+
         for(int i=nums.size()-2; i>=0; i--) {
             if(nums[i] < nums[i+1]) {
-                pivot = i;
+                idx = i;
                 break;
             }
         }
 
-        if(pivot == -1) {
+        if(idx == -1) {
             reverse(nums.begin(), nums.end());
             return;
         }
 
-        int succ;
-        for(int i=nums.size(); i>pivot; i--) {
-            if(nums[i] > nums[pivot]) {
-                succ = i;
+        for(int i=nums.size()-1; i>=idx; i--) {
+            if(nums[i] > nums[idx]) {
+                swap(nums[i], nums[idx]);
                 break;
             }
         }
 
-        swap(nums[pivot], nums[succ]);
-        reverse(nums.begin()+pivot+1, nums.end());
+        reverse(nums.begin() + idx + 1, nums.end());
     }
 };
