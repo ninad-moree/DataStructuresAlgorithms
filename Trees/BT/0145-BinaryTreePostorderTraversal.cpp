d9d228ca538210cd8fd7bf1ff0b82c23a2dc7f85
ninad-moree <ninadyogeshmore@gmail.com>
2025-08-16 11:36:37 +0530

    Binary Tree Postorder Traversal

--- Trees/BT/0145-BinaryTreePostorderTraversal.cpp
+++ Trees/BT/0145-BinaryTreePostorderTraversal.cpp
@@ -34,4 +34,33 @@ public:
         postorder(root, pos);
         return pos;
     }
+
+    vector<int> postorderTraversal2(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st1, st2;
+
+        if(!root)
+            return ans;
+
+        st1.push(root);
+
+        while(!st1.empty()) {
+            auto node = st1.top();
+            st1.pop();
+
+            st2.push(node);
+
+            if(node->left != NULL)
+                st1.push(node->left);
+            if(node->right != NULL)
+                st1.push(node->right);
+        }
+
+        while(!st2.empty()) {
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+
+        return ans;
+    }
 };
