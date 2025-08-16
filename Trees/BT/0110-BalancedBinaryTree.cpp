7a62c908436ce63ac6935266aceec41e6b18e32b
ninad-moree <ninadyogeshmore@gmail.com>
2025-08-16 12:35:30 +0530

    Balanced Binary Tree

--- Trees/BT/0110-BalancedBinaryTree.cpp
+++ Trees/BT/0110-BalancedBinaryTree.cpp
@@ -20,19 +20,41 @@ struct TreeNode {
 
 class Solution {
 public:
-    int height(TreeNode* root) {
+    int maxHeight(TreeNode* root) {
         if(!root)
             return 0;
-        return 1 + max(height(root->left), height(root->right));
+
+        int leftHt = maxHeight(root->left);
+        if(leftHt == -1)
+            return -1;
+        
+        int rightHt = maxHeight(root->right);
+        if(rightHt == -1)
+            return -1;
+
+        if(abs(leftHt - rightHt) > 1)
+            return -1;
+
+        return 1 + max(leftHt, rightHt);
     }
 
     bool isBalanced(TreeNode* root) {
-        if(!root)
-            return true;
-
-        int lh = height(root->left);
-        int rh = height(root->right);
-
-        return (abs(lh - rh) <=1 && isBalanced(root->left) && isBalanced(root->right));
+        return maxHeight(root) != -1;
     }
+
+    // int height(TreeNode* root) {
+    //     if(!root)
+    //         return 0;
+    //     return 1 + max(height(root->left), height(root->right));
+    // }
+
+    // bool isBalanced(TreeNode* root) {
+    //     if(!root)
+    //         return true;
+
+    //     int lh = height(root->left);
+    //     int rh = height(root->right);
+
+    //     return (abs(lh - rh) <=1 && isBalanced(root->left) && isBalanced(root->right));
+    // }
 };
