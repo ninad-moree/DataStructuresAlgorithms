8c9ddcc421d4a94a9460852250929e112e637f55
ninad-moree <ninadyogeshmore@gmail.com>
2025-09-02 20:53:12 +0530

    Find the Number of Ways to Place People I

--- Multidimensional Arrays/3025-FindNumberOfWaysToPlacePeople-1.cpp
+++ Multidimensional Arrays/3025-FindNumberOfWaysToPlacePeople-1.cpp
@@ -1,18 +1,13 @@
 /*
-    You are given a 2D array points of size n x 2 representing integer coordinates of some points on a 2D-plane, 
-    where points[i] = [xi, yi].
-    We define the right direction as positive x-axis (increasing x-coordinate) and the left direction as negative 
-    x-axis (decreasing x-coordinate). Similarly, we define the up direction as positive y-axis (increasing y-coordinate) 
-    and the down direction as negative y-axis (decreasing y-coordinate)
-    You have to place n people, including Chisato and Takina, at these points such that there is exactly one person at 
-    every point. Chisato wants to be alone with Takina, so Chisato will build a rectangular fence with Chisato's position 
-    as the upper left corner and Takina's position as the lower right corner of the fence (Note that the fence might not 
-    enclose any area, i.e. it can be a line). If any person other than Chisato and Takina is either inside the fence or on the 
-    fence, Chisato will be sad.
-    Return the number of pairs of points where you can place Chisato and Takina, such that Chisato does not become sad on 
-    building the fence.
-    Note that Chisato can only build a fence with Chisato's position as the upper left corner, and Takina's position as the lower 
-    right corner. 
+    You are given a 2D array points of size n x 2 representing integer coordinates of some points on a 2D-plane, where points[i] = [xi, yi]. We define 
+    the right direction as positive x-axis (increasing x-coordinate) and the left direction as negative x-axis (decreasing x-coordinate). Similarly, we
+    define the up direction as positive y-axis (increasing y-coordinate) and the down direction as negative y-axis (decreasing y-coordinate) You have to
+    place n people, including Chisato and Takina, at these points such that there is exactly one person at every point. Chisato wants to be alone with 
+    Takina, so Chisato will build a rectangular fence with Chisato's position as the upper left corner and Takina's position as the lower right corner 
+    of the fence (Note that the fence might not enclose any area, i.e. it can be a line). If any person other than Chisato and Takina is either inside
+    the fence or on the fence, Chisato will be sad. Return the number of pairs of points where you can place Chisato and Takina, such that Chisato does
+    not become sad on building the fence. Note that Chisato can only build a fence with Chisato's position as the upper left corner, and Takina's 
+    position as the lower right corner. 
 
     Example 1:
     Input: points = [[1,1],[2,2],[3,3]]
@@ -41,10 +36,12 @@ public:
             for(int j=i+1; j<points.size(); j++) {
                 if(points[i][1] >= points[j][1]) {
                     bool isTrue = true;
+
                     for(int k=i+1; k<j; k++) {
                         if(points[k][1] <= points[i][1] && points[k][1] >= points[j][1])
                             isTrue = false;
                     }
+
                     if(isTrue)
                         ans++;
                 }
