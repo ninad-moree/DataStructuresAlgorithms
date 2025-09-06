522549f676b3c95d1fd401a7723291e5a28da82a
ninad-moree <ninadyogeshmore@gmail.com>
2025-09-06 11:14:40 +0530

    Recover Binary Search Tree

--- Trees/BST/0099-RecoverBinarySearchTree.cpp
+++ Trees/BST/0099-RecoverBinarySearchTree.cpp
@@ -22,51 +22,36 @@ struct TreeNode {
 
 class Solution {
 public:
-    vector<int> ino;
+    TreeNode* first, *middle, *last, *prev;
+
     void inorder(TreeNode* root) {
         if(!root)
             return;
 
         inorder(root->left);
-        ino.push_back(root->val);
+
+        if(prev != NULL && (root->val < prev->val)) {
+            if(first == NULL) {
+                first = prev;
+                middle = root;
+            } else
+                last = root;
+        }
+
+        prev = root;
+
         inorder(root->right);
     }
 
-    TreeNode* searchTree(TreeNode* root, int target) {
-        if(!root)
-            return nullptr;
-
-        if(root->val == target)
-            return root;
-
-        TreeNode* left = searchTree(root->left, target);
-        if(left)
-            return left;
-        return searchTree(root->right, target);
-    }
-
     void recoverTree(TreeNode* root) {
+        first = last = middle = NULL;
+        prev = new TreeNode(INT_MIN);
+
         inorder(root);
 
-        vector<int> corrIno = ino;
-        sort(corrIno.begin(), corrIno.end());
-
-        int first = INT_MIN, second = INT_MIN;
-
-        for(int i=0; i<ino.size(); i++) {
-            if(ino[i] != corrIno[i]) {
-                if (first == INT_MIN)
-                    first = ino[i];
-                else {
-                    second = ino[i];
-                    break;
-                }
-            }
-        }
-
-        TreeNode* f = searchTree(root, first);
-        TreeNode* s = searchTree(root, second);
-
-        swap(f->val, s->val);
+        if(first && last)
+            swap(first->val, last->val);
+        else if(first && middle)
+            swap(first->val, middle->val);
     }
 };
