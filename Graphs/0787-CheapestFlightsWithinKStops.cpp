9a6b64ab9a088f2cf07ae30c0a808335f87050fe
ninad-moree <ninadyogeshmore@gmail.com>
2025-10-01 15:00:57 +0530

    Cheapest Flights Within K Stops

--- Graphs/0787-CheapestFlightsWithinKStops.cpp
+++ Graphs/0787-CheapestFlightsWithinKStops.cpp
@@ -19,42 +19,43 @@ using namespace std;
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist(n, INT_MAX);
         vector<vector<pair<int, int>>> adj(n);
-
         for(int i=0; i<flights.size(); i++) {
             int u = flights[i][0];
             int v = flights[i][1];
-            int d = flights[i][2];
+            int dist = flights[i][2];
 
-            adj[u].push_back({v, d});
+            adj[u].push_back({v, dist});
         }
 
-        queue<pair<int, pair<int, int>>> q;
-        q.push({src, {0, 0}});
+        queue<pair<int, pair<int, int>>> q; // {stops, {node, dist}}
+        vector<int> dist(n, INT_MAX);
+
+        q.push({0, {src, 0}});
+        dist[src] = 0;
 
         while(!q.empty()) {
-            auto f = q.front();
+            int stops = q.front().first;
+            int node = q.front().second.first;
+            int dis = q.front().second.second;
             q.pop();
-
-            int node = f.first;
-            int stops = f.second.first;
-            int weight = f.second.second;
 
             if(stops > k)
                 continue;
 
             for(auto i : adj[node]) {
                 int v = i.first;
-                int w = i.second;
+                int wt = i.second;
 
-                if(weight + w < dist[v] && stops <= k) {
-                    dist[v] = w + weight;
-                    q.push({v, {stops+1, dist[v]}});
+                if(wt + dis < dist[v]) {
+                    dist[v] = wt + dis;
+                    q.push({stops+1, {v, dist[v]}});
                 }
             }
         }
-        
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+
+        if(dist[dst] == INT_MAX)
+            return -1;
+        return dist[dst];
     }
 };
