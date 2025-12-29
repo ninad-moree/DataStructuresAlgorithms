e357c2e10cd0af9385215c0d9121399f9979c7e8
ninad-moree <ninadyogeshmore@gmail.com>
2025-12-29 16:20:09 +0530

    Combination Sum 2

--- Backtracking/0040-CombinationSum2.cpp
+++ Backtracking/0040-CombinationSum2.cpp
@@ -1,43 +1,49 @@
 /*
-    Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate
-    numbers sum to target. Each number in candidates may only be used once in the combination. Note: The solution set must not contain duplicate 
-    combinations.
+    Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
+    Each number in candidates may only be used once in the combination. Note: The solution set must not contain duplicate combinations.
 
     Example 1:
+
     Input: candidates = [10,1,2,7,6,1,5], target = 8
-    Output: [[1,1,6], [1,2,5], [1,7], [2,6]]
+    Output: 
+    [
+        [1,1,6],
+        [1,2,5],
+        [1,7],
+        [2,6]
+    ]
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    void findCombination(int ind, int target, vector <int>& arr, vector<vector<int>> & ans, vector<int>& ds) {
-        if (target == 0) {
-            ans.push_back(ds);
+    void solve(int index, vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& arr) {
+        if(target == 0) {
+            ans.push_back(arr);
             return;
         }
 
-        for (int i = ind; i < arr.size(); i++) {
-            if (i > ind && arr[i] == arr[i - 1]) 
+        for(int i=index; i<candidates.size(); i++) {
+            if(i > index && candidates[i] == candidates[i-1])
                 continue;
 
-            if (arr[i] > target) 
+            if(candidates[i] > target)
                 break;
 
-            ds.push_back(arr[i]);
-            findCombination(i + 1, target - arr[i], arr, ans, ds);
-            ds.pop_back();
+            arr.push_back(candidates[i]);
+            solve(i+1, candidates, target - candidates[i], ans, arr);
+            arr.pop_back();
         }
     }
-    
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-        vector <vector<int>> ans;
-        vector < int > ds;
-        
-        findCombination(0, target, candidates, ans, ds);
+        vector<vector<int>> ans;
+        vector<int> arr;
+
+        solve(0, candidates, target, ans, arr);
 
         return ans;
     }
