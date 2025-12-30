3bf5457bc6a026db6c42572a75875ed5350e1460
ninad-moree <ninadyogeshmore@gmail.com>
2025-12-30 15:44:09 +0530

    Palindrome Partitioning

--- Backtracking/0131-PalindromePartitioning.cpp
+++ Backtracking/0131-PalindromePartitioning.cpp
@@ -11,36 +11,39 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPalindrome(string s, int i, int j) {
-        while(i < j) {
-            if(s[i]!=s[j]) 
+    bool isPalindrome(string s, int start, int end) {
+        while(start < end) {
+            if(s[start] != s[end])
                 return false;
-            i++;
-            j--;
+            start++;
+            end--;
         }
-        
+
         return true;
     }
 
-    void findPartitions(const string& s, int start, vector<string>& current, vector<vector<string>>& result) {
-        if (start == s.size()) {
-            result.push_back(current);
+    // INDIVIDAL STRING PARTITIONS --> PATH
+    void solve(int index, string s, vector<string>& path, vector<vector<string>>& ans) {
+        if(index == s.size()) {
+            ans.push_back(path);
             return;
         }
 
-        for (int end = start; end < s.size(); end++) {
-            if (isPalindrome(s, start, end)) {
-                current.push_back(s.substr(start, end - start + 1));
-                findPartitions(s, end + 1, current, result);
-                current.pop_back();
+        for(int i=index; i<s.size(); i++) {
+            if(isPalindrome(s, index, i)) {
+                path.push_back(s.substr(index, i-index+1));
+                solve(i+1, s, path, ans);
+                path.pop_back();
             }
         }
     }
 
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
-        vector<string> current;
-        findPartitions(s, 0, current, ans);
+        vector<string> path;
+
+        solve(0, s, path, ans);
+
         return ans;
     }
 };
