d2ae175f774f153531f3ca4476cb5cc5995fe8d9
ninad-moree <ninadyogeshmore@gmail.com>
2026-01-01 11:43:42 +0530

    Plus One

--- Math/0066-PlusOne.cpp
+++ Math/0066-PlusOne.cpp
@@ -19,17 +19,19 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
+
         for(int i=n-1; i>=0; i--) {
             if(digits[i]<9) {
                 digits[i]++;
                 return digits;
             }
-            else {
+            else 
                 digits[i]=0;
-            }
         }
+
         digits[0]=1;
         digits.push_back(0);
+        
         return digits;
     }
 };
