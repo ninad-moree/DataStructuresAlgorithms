99c3c46d63288c54cb9e5012d5ef5e0fb7c511e7
ninad-moree <ninadyogeshmore@gmail.com>
2026-01-02 19:41:09 +0530

    Climbing Stairs

--- Dynamic Programming/0070-ClimbingStairs.cpp
+++ Dynamic Programming/0070-ClimbingStairs.cpp
@@ -1,6 +1,5 @@
 /*
-    You are climbing a staircase. It takes n steps to reach the top.
-    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    You are climbing a staircase. It takes n steps to reach the top. Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
     Example 1:
     Input: n = 2
@@ -16,15 +15,15 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) 
-            return n;
+        if(n <= 1)
+            return 1;
 
-        vector<int> dp(n + 1, 0);
+        vector<int> dp(n+1, -1);
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
 
-        for (int i = 3; i <= n; ++i) 
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for(int i=2; i<=n; i++) 
+            dp[i] = dp[i-1] + dp[i-2];
 
         return dp[n];
     }
