520e188991edc74a93d28f2362b6bd1a4213c8cc
ninad-moree <ninadyogeshmore@gmail.com>
2026-01-07 17:44:59 +0530

    Minimum Path Sum

--- Dynamic Programming/0064-MinimumPathSum.cpp
+++ Dynamic Programming/0064-MinimumPathSum.cpp
@@ -13,26 +13,28 @@ using namespace std;
 
 class Solution {
 public:
+    int solve(vector<vector<int>>& grid, vector<vector<int>>& dp, int row, int col) {
+        if(row == 0 && col == 0)
+            return dp[0][0] = grid[0][0]; 
+        if(row < 0 || col < 0)
+            return 1000000;
+
+        if(dp[row][col] != -1)
+            return dp[row][col];
+
+        int up = grid[row][col] + solve(grid, dp, row-1, col);
+        int left = grid[row][col] + solve(grid, dp, row, col-1);
+
+        return dp[row][col] = min(up, left);
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        vector<vector<int>> dp(n, vector<int>(m));
+        solve(grid, dp, m-1, n-1);
 
-        dp[0][0] = grid[0][0];
-
-        for(int i=1; i<n; i++) 
-            dp[i][0] = grid[i][0] + dp[i-1][0];
-
-        for(int i=1; i<m; i++)
-            dp[0][i] = grid[0][i] + dp[0][i-1];
-
-        for(int i=1; i<n; i++) {
-            for(int j=1; j<m; j++) {
-                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-
-        return dp[n-1][m-1];
+        return dp[m-1][n-1];
     }
 };
