c8726bccab342208fed28192a5a0a03813547cce
ninad-moree <ninadyogeshmore@gmail.com>
2026-01-07 16:10:46 +0530

    Unique Paths 2

--- Dynamic Programming/0063-UniquePaths2.cpp
+++ Dynamic Programming/0063-UniquePaths2.cpp
@@ -19,40 +19,33 @@ using namespace std;
 
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
-
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[n-1][m-1] == 1)
+    int solve(vector<vector<int>>& grid, vector<vector<int>>& dp, int row, int col) {
+        if(row < 0 || col < 0)
             return 0;
 
-        dp[0][0] = 1;
+        if(grid[row][col] == 1)
+            return 0;
 
-        for(int i=1; i<n; i++) {
-            if(obstacleGrid[i][0] == 1)
-                dp[i][0] = 0;
-            else
-                dp[i][0] = dp[i-1][0];
-        }
+        if(row == 0 && col == 0)
+            return 1;
 
-        for(int i=1; i<m; i++) {
-            if(obstacleGrid[0][i] == 1)
-                dp[0][i] = 0;
-            else
-                dp[0][i] = dp[0][i-1];
-        }
+        if(dp[row][col] != -1)
+            return dp[row][col];
 
-        for(int i=1; i<n; i++) {
-            for(int j=1; j<m; j++) {
-                if(obstacleGrid[i][j] == 1)
-                    dp[i][j] = 0;
-                else
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
-        }
+        int up = solve(grid, dp, row - 1, col);
+        int left = solve(grid, dp, row, col - 1);
 
-        return dp[n-1][m-1];
+        return dp[row][col] = up + left;
+    }
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1)
+            return 0;
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(obstacleGrid, dp, m - 1, n - 1);
     }
 };
