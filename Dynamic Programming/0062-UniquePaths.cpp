be5061af2a64f781734c064b441da6359de346e7
ninad-moree <ninadyogeshmore@gmail.com>
2026-01-07 15:53:47 +0530

    Unique Paths

--- Dynamic Programming/0062-UniquePaths.cpp
+++ Dynamic Programming/0062-UniquePaths.cpp
@@ -14,19 +14,27 @@ using namespace std;
 
 class Solution {
 public:
+    int solve(int row, int col, vector<vector<int>>& dp) {
+        if(row == 0 && col == 0)
+            return 1;
+        if(row < 0 || col < 0)
+            return 0;
+
+        if(dp[row][col] != -1)
+            return dp[row][col];
+
+        int up = solve(row-1, col, dp);
+        int left = solve(row, col-1, dp);
+
+        return dp[row][col] = (up+left);
+    }
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        if(m == 1 && n == 1)
+            return 1;
 
-        for(int i=0; i<m; i++)
-            dp[i][0] = 1;
-
-        for(int i=0; i<n; i++)
-            dp[0][i] = 1;
-
-        for(int i=1; i<m; i++) {
-            for(int j=1; j<n; j++) 
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-        }
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        solve(m-1, n-1, dp);
 
         return dp[m-1][n-1];
     }
