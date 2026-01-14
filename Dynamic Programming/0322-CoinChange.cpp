147c1b2bacc57424214d0de0767bca6215ae2134
ninad-moree <ninadyogeshmore@gmail.com>
2026-01-14 18:46:02 +0530

    Coin Change

--- Dynamic Programming/0322-CoinChange.cpp
+++ Dynamic Programming/0322-CoinChange.cpp
@@ -14,20 +14,35 @@ using namespace std;
 
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1, INT_MAX);
-
-        dp[0] = 0;
-
-        for(int i=1; i<=amount; i++) {
-            for(int j=0; j<coins.size(); j++) {
-                if(i - coins[j] >=0 && dp[i - coins[j]] != INT_MAX)
-                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
-            }
+    int solve(int index, int amount, vector<int>& coins, vector<vector<int>>& dp) {
+        if(index == 0) {
+            if(amount % coins[index] == 0)
+                return  amount /  coins[index];
+            else
+                return 1e9;
         }
 
-        if(dp[amount] == INT_MAX)
-            return -1;
-        return dp[amount];
+        if(dp[index][amount] != -1)
+            return dp[index][amount];
+
+        int notTake = solve(index-1, amount, coins, dp);
+        int take = 1e9;
+        if(amount >= coins[index]) 
+            take = 1 + solve(index, amount - coins[index], coins, dp);
+        
+
+        return dp[index][amount] = min(notTake, take);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        if(amount == 0)
+            return 0;
+
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, 0));
+
+        int ans = solve(n-1, amount, coins, dp);
+
+        return (ans >= 1e9) ? -1 : ans;
     }
 };
