b80409bbc888456d05b173b9b614de59546ddf54
ninad-moree <ninadyogeshmore@gmail.com>
2026-01-24 12:19:08 +0530

    Best Time to Buy and Sell Stock with Transaction Fee

--- Dynamic Programming/0714-BestTimeToBuySndSellStockWithTransactionFee.cpp
+++ Dynamic Programming/0714-BestTimeToBuySndSellStockWithTransactionFee.cpp
@@ -20,32 +20,28 @@ using namespace std;
 
 class Solution {
 public:
-    int solve(int idx, int canBuy, int fee, vector<int>& prices, vector<vector<int>>& dp) {
-        if(idx >= prices.size())
-            return 0;
+    int maxProfit(vector<int>& prices, int fee) {
+        int n = prices.size();
+        vector<vector<int>> dp(n+1, vector<int>(2, 0));
 
         int profit = 0;
 
-        if(dp[idx][canBuy] != -1)
-            return dp[idx][canBuy];
+        for(int i=n-1; i>=0; i--) {
+            for(int canBuy=0; canBuy<=1; canBuy++) {
+                if(canBuy) {
+                    int buy = -prices[i] + dp[i+1][0];
+                    int notBuy = dp[i+1][1];
+                    profit = max(buy, notBuy);
+                } else {
+                    int sell = prices[i] - fee + dp[i+1][1];
+                    int notSell = dp[i+1][0];
+                    profit = max(sell, notSell);
+                }
 
-        if(canBuy) {
-            int buy = -prices[idx] + solve(idx+1, 0, fee, prices, dp);
-            int noBuy = 0 + solve(idx+1, 1, fee, prices, dp);
-            profit = max(buy, noBuy);
-        } else {
-            int sell = prices[idx] - fee + solve(idx+1, 1, fee, prices, dp);
-            int noSell = 0 + solve(idx+1, 0, fee, prices, dp);
-            profit = max(sell, noSell);
+                dp[i][canBuy] = profit;
+            }
         }
 
-        return dp[idx][canBuy] = profit;
-    }
-
-    int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-
-        return solve(0, 1, fee, prices, dp);
+        return dp[0][1];
     }
 };
