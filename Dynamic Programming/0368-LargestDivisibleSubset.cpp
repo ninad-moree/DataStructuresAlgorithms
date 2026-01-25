7dc10f90661d1aa42dc392220967d5886da40fdb
ninad-moree <ninadyogeshmore@gmail.com>
2026-01-25 12:48:59 +0530

    Largest Divisible Subset

--- Dynamic Programming/0368-LargestDivisibleSubset.cpp
+++ Dynamic Programming/0368-LargestDivisibleSubset.cpp
@@ -1,8 +1,6 @@
 /*
-    Given a set of distinct positive integers nums, return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this 
-    subset satisfies:
-    answer[i] % answer[j] == 0, or answer[j] % answer[i] == 0
-    If there are multiple solutions, return any of them.
+    Given a set of distinct positive integers nums, return the largest subset answer such that every pair (answer[i], answer[j]) of elements in this subset satisfies:
+    answer[i] % answer[j] == 0, or answer[j] % answer[i] == 0. If there are multiple solutions, return any of them.
 
     Example 1:
     Input: nums = [1,2,3]
@@ -16,39 +14,41 @@ using namespace std;
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        if (nums.empty()) {
-            return {};
-        }
-
         sort(nums.begin(), nums.end());
 
         int n = nums.size();
+        int maxi = 0;
+        int lastIdx = 0;
         vector<int> dp(n, 1);
-        vector<int> parent(n, -1);
-        int maxLen = 1, maxIndex = 0;
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
+        vector<int> hash(n, 0);
+        
+        for(int i=0; i<n; i++)
+            hash[i] = i;
+        
+        for(int i=0; i<n; i++) {
+            for(int prev=0; prev<i; prev++) {
+                if(nums[i]%nums[prev] == 0  && 1 + dp[prev] > dp[i]) {
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
                 }
             }
-
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                maxIndex = i;
+            
+            if(dp[i] > maxi) {
+                maxi = dp[i];
+                lastIdx = i;
             }
+
+            maxi = max(maxi, dp[i]);
         }
-
-        vector<int> result;
-        while (maxIndex != -1) {
-            result.push_back(nums[maxIndex]);
-            maxIndex = parent[maxIndex];
+        
+        vector<int> ans;
+        ans.push_back(nums[lastIdx]);
+        while(hash[lastIdx] != lastIdx) {
+            lastIdx = hash[lastIdx];
+            ans.push_back(nums[lastIdx]);
         }
-
-        reverse(result.begin(), result.end());
-
-        return result;
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
