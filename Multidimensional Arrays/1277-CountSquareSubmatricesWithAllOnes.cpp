077692498e4a66557a8d3ef84273ca735e34b39e
ninad-moree <ninadyogeshmore@gmail.com>
2026-02-08 11:57:34 +0530

    Count Square Submatrices with All Ones

--- Multidimensional Arrays/1277-CountSquareSubmatricesWithAllOnes.cpp
+++ Multidimensional Arrays/1277-CountSquareSubmatricesWithAllOnes.cpp
@@ -9,11 +9,7 @@
         [0,1,1,1]
     ]
     Output: 15
-    Explanation: 
-    There are 10 squares of side 1.
-    There are 4 squares of side 2.
-    There is  1 square of side 3.
-    Total number of squares = 10 + 4 + 1 = 15.
+    Explanation:  There are 10 squares of side 1. There are 4 squares of side 2. There is  1 square of side 3. Total number of squares = 10 + 4 + 1 = 15.
 */
 
 #include<bits/stdc++.h>
@@ -22,35 +18,28 @@ using namespace std;
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m));
+
+        for(int i=0; i<n; i++)
+            dp[i][0] = matrix[i][0];
+
+        for(int j=0; j<m; j++)
+            dp[0][j] = matrix[0][j];
+
+        for(int i=1; i<n; i++) {
+            for(int j=1; j<m; j++) {
+                if(matrix[i][j] == 1)
+                    dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
+            }
+        }
 
         int ans = 0;
-
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                int maxi = min(m-i, n-j);
-
-                for(int k=1; k<=maxi; k++) {
-                    bool ones = true;
-
-                    for(int x=0; x<k; x++) {
-                        for(int y=0; y<k; y++) {
-                            if(matrix[i+x][j+y] != 1) {
-                                ones = false;
-                                break;
-                            }
-                        }
-                        if(!ones)
-                            break;
-                    }   
-
-                    if(ones)
-                        ans++;
-                    else
-                        break;
-                }
-            }
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) 
+                ans += dp[i][j];
         }
 
         return ans;
