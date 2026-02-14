a166c51ba3cef3c5fc900d9eb8d78f8d219548c2
ninad-moree <ninadyogeshmore@gmail.com>
2026-02-14 11:54:30 +0530

    Bitwise AND of Numbers Range

--- BitManipulation/0201-BitwiseAndOfNumbersRange.cpp
+++ BitManipulation/0201-BitwiseAndOfNumbersRange.cpp
@@ -1,6 +1,5 @@
 /*
-    Given two integers left and right that represent the range [left, right], return the bitwise AND of all 
-    numbers in this range, inclusive.
+    Given two integers left and right that represent the range [left, right], return the bitwise AND of all  numbers in this range, inclusive.
 
     Example 1:
     Input: left = 5, right = 7
