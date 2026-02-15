c2b9a3d4ae3350fb8bc70e36c1e628d08d9978df
ninad-moree <ninadyogeshmore@gmail.com>
2026-02-15 12:15:13 +0530

    Subsets

--- Recusion-Backtracking/0078-Subsets.cpp
+++ Recusion-Backtracking/0078-Subsets.cpp
@@ -36,4 +36,25 @@ public:
 
         return ans;
     }
+
+    /* BIT MANIPULATION*/
+    vector<vector<int>> subsetsBit(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        int subSets = 1 << n;
+
+        for(int num=0; num<subSets; num++) {
+            vector<int> res;
+
+            for(int i=0; i<n; i++) {
+                if(num & (1 << i))
+                    res.push_back(nums[i]);
+            }
+
+            ans.push_back(res);
+        }
+
+        return ans;
+    }
 };
