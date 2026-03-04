9eeaef05a26f828f55b4869fe5f23b5eb0764999
ninad-moree <ninadyogeshmore@gmail.com>
2026-03-04 14:52:05 +0530

    Number of Substrings Containing All Three Characters

--- Strings/1358-NumOfSubstringsContainingAllThreeCharacters.cpp
+++ Strings/1358-NumOfSubstringsContainingAllThreeCharacters.cpp
@@ -15,21 +15,17 @@ using namespace std;
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int cnt = 0;
-        unordered_map<char, int> mp;
-        int i = 0;
+        vector<int> lastSeen(3, -1);
 
-        for(int j=0;j<s.size();j++) {
-            mp[s[j]]++;
+        int ans = 0;
 
-            while(mp['a'] && mp['b'] && mp['c']) {
-                mp[s[i]]--;
-                i++;
-            }
+        for(int i=0; i<s.size(); i++) {
+            lastSeen[s[i] - 'a'] = i;
 
-            cnt += i;
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1)
+                ans = ans + (1 + min(lastSeen[0], min(lastSeen[1], lastSeen[2])));    
         }
 
-        return cnt;
+        return ans;
     }
 };
