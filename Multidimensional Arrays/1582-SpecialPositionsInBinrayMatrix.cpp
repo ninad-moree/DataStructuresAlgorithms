d24b7937aff1de8b2d01096a61dca055cc42ef0c
ninad-moree <ninadyogeshmore@gmail.com>
2026-03-04 11:21:27 +0530

    Special Positions in a Binary Matrix

--- Multidimensional Arrays/1582-SpecialPositionsInBinrayMatrix.cpp
+++ Multidimensional Arrays/1582-SpecialPositionsInBinrayMatrix.cpp
@@ -15,28 +15,29 @@ using namespace std;
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int rowCount = mat.size();
-        int colCount = mat[0].size();
-        int specialCount = 0;
+        vector<int> row(mat.size(), 0);
+        vector<int> col(mat[0].size(), 0);
 
-        vector<int> rowSum(rowCount, 0);
-        vector<int> colSum(colCount, 0);
-
-        for (int i = 0; i < rowCount; ++i) {
-            for (int j = 0; j < colCount; ++j) {
-                rowSum[i] += mat[i][j];
-                colSum[j] += mat[i][j];
-            }
-        }
-
-        for (int i = 0; i < rowCount; ++i) {
-            for (int j = 0; j < colCount; ++j) {
-                if (mat[i][j] == 1 && rowSum[i] == 1 && colSum[j] == 1) {
-                    specialCount++;
+        for(int i=0; i<mat.size(); i++) {
+            for(int j=0; j<mat[0].size(); j++) {
+                if(mat[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
                 }
             }
         }
 
-        return specialCount;
+        int ans = 0;
+
+        for(int i=0; i<mat.size(); i++) {
+            for(int j=0; j<mat[0].size(); j++) {
+                if(mat[i][j] == 1) {
+                    if(row[i] == 1 && col[j] == 1)
+                        ans++;
+                }
+            }
+        }
+
+        return ans;
     }
 };
