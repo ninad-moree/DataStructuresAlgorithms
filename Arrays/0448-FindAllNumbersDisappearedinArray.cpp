04b515e49033c5fb3efd96dfe3b67f3c0d87f5ec
ninad-moree <ninadyogeshmore@gmail.com>
2026-03-27 14:35:03 +0530

    Find All Numbers Disappeared in an Array

--- Arrays/0448-FindAllNumbersDisappearedinArray.cpp
+++ Arrays/0448-FindAllNumbersDisappearedinArray.cpp
@@ -13,15 +13,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> hash(n+1);
+
+        for(int i=0; i<n; i++) 
+            hash[nums[i]]++;
+        
         vector<int> ans;
-
-        vector<int> freq(nums.size()+1, 0);
-
-        for(int i=0; i<nums.size(); i++) 
-            freq[nums[i]]++;
-
-        for(int i=1; i<freq.size(); i++) {
-            if(freq[i] == 0)
+        for(int i=1; i<n+1; i++) {
+            if(hash[i] == 0)
                 ans.push_back(i);
         }
 
