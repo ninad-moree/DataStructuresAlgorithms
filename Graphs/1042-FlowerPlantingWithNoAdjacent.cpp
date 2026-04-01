db1c90b71260531c4cd06bf10eb8219adaee38e1
ninad-moree <ninadyogeshmore@gmail.com>
2026-04-01 17:13:23 +0530

    Flower Planting With No Adjacent

--- Graphs/1042-FlowerPlantingWithNoAdjacent.cpp
+++ Graphs/1042-FlowerPlantingWithNoAdjacent.cpp
@@ -1,8 +1,8 @@
 /*
-    You have n gardens, labeled from 1 to n, and an array paths where paths[i] = [xi, yi] describes a bidirectional path between garden xi to garden yi. In each garden, you want to
-    plant one of 4 types of flowers. All gardens have at most 3 paths coming into or leaving it. Your task is to choose a flower type for each garden such that, for any two gardens
-    connected by a path, they have different types of flowers. Return any such a choice as an array answer, where answer[i] is the type of flower planted in the (i+1)th garden. The
-    flower types are denoted 1, 2, 3, or 4. It is guaranteed an answer exists.
+    You have n gardens, labeled from 1 to n, and an array paths where paths[i] = [xi, yi] describes a bidirectional path between garden xi to garden yi. In each garden, you want
+    to plant one of 4 types of flowers. All gardens have at most 3 paths coming into or leaving it. Your task is to choose a flower type for each garden such that, for any two 
+    gardens connected by a path, they have different types of flowers. Return any such a choice as an array answer, where answer[i] is the type of flower planted in the (i+1)th 
+    garden. The flower types are denoted 1, 2, 3, or 4. It is guaranteed an answer exists.
 
     Example 1:
     Input: n = 3, paths = [[1,2],[2,3],[3,1]]
