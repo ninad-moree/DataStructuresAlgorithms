49c7f37ed14f0c2b832f108e7d6475ce565b3e7e
ninad-moree <ninadyogeshmore@gmail.com>
2026-08-09 12:08:16 +0530

    Merge Strings Alternately

--- Strings/1768-MergeStringsAlternately.cpp
+++ Strings/1768-MergeStringsAlternately.cpp
@@ -21,24 +21,29 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string ans = "";
+
         int n = word1.size();
         int m = word2.size();
         int i=0;
         int j=0;
+
         while(i<n && j<m) {
             ans += word1[i];
             i++;
             ans+=word2[j];
             j++;
         }
+
         while(i<n) {
             ans += word1[i];
             i++;
         }
+
         while(j<m) {
             ans += word2[j];
             j++;
         }
+        
         return ans;
     }
 };
