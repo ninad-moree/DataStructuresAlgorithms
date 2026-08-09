aac954acbe2aa44523232f60803c9ea4b980dea4
ninad-moree <ninadyogeshmore@gmail.com>
2026-08-09 12:09:29 +0530

    Redistribute Characters to Make All Strings Equal

--- Strings/1897-RedistributeCharactersToMakeAllStringsEqual.cpp
+++ Strings/1897-RedistributeCharactersToMakeAllStringsEqual.cpp
@@ -1,17 +1,11 @@
 /*
-    You are given an array of strings words (0-indexed).
-    In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, 
-    and move any character from words[i] to any position in words[j].
-    Return true if you can make every string in words equal using any number of operations, 
-    and false otherwise.
+    You are given an array of strings words (0-indexed). In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from 
+    words[i] to any position in words[j]. Return true if you can make every string in words equal using any number of operations,  and false otherwise.
 
     Example 1:
-
     Input: words = ["abc","aabc","bc"]
     Output: true
-    Explanation: Move the first 'a' in words[1] to the front of words[2],
-    to make words[1] = "abc" and words[2] = "abc".
-    All the strings are now equal to "abc", so return true
+    Explanation: Move the first 'a' in words[1] to the front of words[2], to make words[1] = "abc" and words[2] = "abc". All the strings are now equal to "abc", so return true
 */
 
 #include<bits/stdc++.h>
@@ -32,9 +26,8 @@ public:
 
         for(int i=0; i<n; i++) {
             string str = words[i];
-            for(int j=0;j<str.size();j++) {
+            for(int j=0;j<str.size();j++) 
                 vec[str[j]-'a']++;
-            }
         }
 
         for (int i = 0; i < vec.size(); i++) {
