df661d9818e2985d9d4f85d7a16defe87773ba95
ninad-moree <ninadyogeshmore@gmail.com>
2026-08-09 12:04:57 +0530

    Largest 3-Same-Digit Number in String

--- Strings/2264-Largest3SameDigitNumberInString.cpp
+++ Strings/2264-Largest3SameDigitNumberInString.cpp
@@ -1,14 +1,12 @@
 /*
-    You are given a string num representing a large integer. An integer is good if it meets the following conditions:
-    It is a substring of num with length 3. It consists of only one unique digit.
-    Return the maximum good integer as a string or an empty string "" if no such integer exists.
-    Note: A substring is a contiguous sequence of characters within a string. There may be leading zeroes in num or a good integer.
+    You are given a string num representing a large integer. An integer is good if it meets the following conditions: It is a substring of num with length 3. It consists of only
+    one unique digit.  Return the maximum good integer as a string or an empty string "" if no such integer exists. Note: A substring is a contiguous sequence of characters 
+    within a string. There may be leading zeroes in num or a good integer.
     
     Example 1:
     Input: num = "6777133339"
     Output: "777"
-    Explanation: There are two distinct good integers: "777" and "333".
-    "777" is the largest, so we return "777".
+    Explanation: There are two distinct good integers: "777" and "333". "777" is the largest, so we return "777".
 */
 
 #include<bits/stdc++.h>
