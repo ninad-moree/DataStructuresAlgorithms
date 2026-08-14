add2dd6ff1da6fd69776fb5c248dbd169a640d34
ninad-moree <ninadyogeshmore@gmail.com>
2026-08-14 08:49:20 +0530

    Maximum Length Substring With Two Occurrences

--- Strings/3090-MaximumLengthSubstringWithTwoOccurrences.cpp
+++ Strings/3090-MaximumLengthSubstringWithTwoOccurrences.cpp
@@ -4,8 +4,7 @@
     Example 1:
     Input: s = "bcbbbcba"
     Output: 4
-    Explanation:
-    The following substring has a length of 4 and contains at most two occurrences of each character: "bcbbbcba".
+    Explanation: The following substring has a length of 4 and contains at most two occurrences of each character: "bcbbbcba".
 */
 
 #include<bits/stdc++.h>
@@ -14,24 +13,21 @@ using namespace std;
 class Solution {
 public:
     int maximumLengthSubstring(string s) {
-        unordered_map<char, int> mp;
-
-        int i=0;
-        int j=0;
+        int i = 0;
+        int j = 0;
         int len = 0;
+
+        unordered_map<char, int> mp;
 
         while(j < s.size()) {
             mp[s[j]]++;
-            if(mp[s[j]] <= 2) 
-                len = max(len, j-i+1);
-            else {
-                while(mp[s[j]] > 2) {
-                    mp[s[i]]--;
-                    if(mp[s[i]] == 0)
-                        mp.erase(s[i]);
-                    i++;
-                }
+
+            while(mp[s[j]] > 2) {
+                mp[s[i]]--;
+                i++;
             }
+
+            len = max(len, j - i + 1);
             j++;
         }
 
