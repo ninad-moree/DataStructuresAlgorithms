1a2fe8039556a40ace14ee4d1f306c74c3e8015f
ninad-moree <ninadyogeshmore@gmail.com>
2026-09-16 18:56:35 +0530

    Largest Prime from Consecutive Prime Sum

--- Arrays/3770-LargestPrimeFromConsecutivePrimeSum.cpp
+++ Arrays/3770-LargestPrimeFromConsecutivePrimeSum.cpp
@@ -1,6 +1,6 @@
 /*
-    You are given an integer n.
-    Return the largest prime number less than or equal to n that can be expressed as the sum of one or more consecutive prime numbers starting from 2. If no such number exists, return 0.
+    You are given an integer n. Return the largest prime number less than or equal to n that can be expressed as the sum of one or more consecutive prime numbers starting from 
+    2. If no such number exists, return 0.
 
     Example 1:
     Input: n = 20
